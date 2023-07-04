b2903fb17d82e4f280a3b0db72e8a46c3c633a09
Admin <admin@Chevys-Mac.local>
2023-07-03 23:47:40 -0400

    change

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,13 +12,13 @@ module.exports = {
         'pinky': 'hsl(322, 100%, 66%)',
         'veryPaleCyan': 'hsl(193, 100%, 96%)',
         'veryDarkCyan': 'hsl(192, 100%, 9%)',
-        'grayishBlue': 'hsl(208, 11%, 55%)'
+        'grayishBlue': 'hsl(208, 11%, 55%)',
       },
       fontFamily: {
         sans: ['Poppins', 'sans-serif'],
         open: ['Open Sans', 'sans-serif']
-      }
-    }
+      },
+    },
   },
-  plugins: []
-};
+  plugins: [],
+},
